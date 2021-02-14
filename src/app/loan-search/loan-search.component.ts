531dc2d2ba1462dbe12cdc20aa51a4bbd84af6e7
luisRivera162 <luis.rivera162@upr.edu>
2021-02-14 15:13:57 -0400

    Added the Loan Search component

---
+++ src/app/loan-search/loan-search.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-loan-search',
+  templateUrl: './loan-search.component.html',
+  styleUrls: ['./loan-search.component.css']
+})
+export class LoanSearchComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+  }
+
+}
