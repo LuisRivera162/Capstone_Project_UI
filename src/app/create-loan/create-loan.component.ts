5faa84444997fbb9f1cc82d4b1c26c866b905b50
luisRivera162 <luis.rivera162@upr.edu>
2021-02-15 15:54:52 -0400

    Added post request for creating a loan.

--- src/app/create-loan/create-loan.component.ts
+++ src/app/create-loan/create-loan.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
@@ -8,13 +9,17 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './create-loan.component.html',
   styleUrls: ['./create-loan.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class CreateLoanComponent implements OnInit {
 
   error: string = "null";
 
   constructor(
     private authService: AuthService, 
-    private router: Router
+    private router: Router,
+    private HttpClient: HttpClient
     ) { }
 
   ngOnInit(): void {
@@ -26,16 +31,32 @@ export class CreateLoanComponent implements OnInit {
       this.error = "Form is not valid, make sure you fill all fields."
       return;
     }
+
+    this.error = "null"; 
     
     let loan_amount = form.value.loan_amount;
     let interest = form.value.interest;
     let time_frame = form.value.time_frame;
     let platform = form.value.platform;
+    let user_data: {
+      email: string;
+      id: string;
+    } = JSON.parse(localStorage.getItem('userData') || '{}');
+    if(!user_data.email && !user_data.id){
+      return; 
+    }
 
     console.log(loan_amount);
     console.log(interest);
     console.log(time_frame);
     console.log(platform);
+    console.log(user_data.id);
+
+    return this.HttpClient.post(
+      '/api/create-loan',
+      {loan_amount: loan_amount, interest: interest, time_frame: time_frame,
+      platform: platform, user_id: user_data.id}).subscribe();
+    
   }
 
 }
