9637ac9ef63b18fb583da54bbd5d768e461c8bca
Josian Velez <josian.velez@upr.edu>
2021-03-05 16:52:10 -0400

    Fixed the nav bar blocking top of webpage and implemented settings page

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../auth/auth.service";
 import {Router} from "@angular/router";
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {NgForm} from "@angular/forms";
+
+interface UserResponseData {
+  'user_id': number
+  'username': string
+  'first_name': string
+  'last_name': string
+  'email': string
+  'age': string
+  'phone': string
+}
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
+
 export class SettingsComponent implements OnInit {
 
   constructor(
@@ -16,7 +28,40 @@ export class SettingsComponent implements OnInit {
     private HttpClient: HttpClient
   ) { }
 
+  user_id = this.authService.user.getValue()!.id;
+  user_email = this.authService.user.getValue()!.email;
+  username = ""
+  firstname = ""
+  lastname = ""
+  age = ""
+  phone = ""
+
   ngOnInit(): void {
+    const params = new HttpParams().append('user_id', this.user_id);
+
+    this.HttpClient.get<UserResponseData>(
+      '/api/user',
+      {
+        params
+      }
+    ).subscribe(resData => {
+      this.username = resData.username
+      this.firstname = resData.first_name
+      this.lastname = resData.last_name
+      this.age = resData.age
+      this.phone = resData.phone
+    });
   }
 
+  onSaveChanges(form: NgForm, type: any) {
+
+    if (type == 'profile'){
+
+    }else if (type == 'password'){
+
+    }else {
+
+    }
+
+  }
 }
