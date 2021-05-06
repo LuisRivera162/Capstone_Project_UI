3a1b244012c4a106f307a857a8091e967af15897
luisRivera162 <luis.rivera162@upr.edu>
2021-05-06 09:30:28 -0400

    Fixed some profile edit issues.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {NgForm} from "@angular/forms";
 import {User} from "../auth/user.model";
+import { WSAEINVALIDPROCTABLE } from 'constants';
 
 interface UserResponseData {
   'user_id': number
@@ -45,19 +46,19 @@ export class ProfileComponent implements OnInit {
   ) { }
 
   user_id = this.authService.user.getValue()!.id;
-  passwordError = false
-  passwordChanged = false
+  passwordError = false;
+  passwordChanged = false;
   check: string = "True";
   email = this.authService.user.getValue()!.email;
-  username = ""
-  firstname = ""
-  lastname = ""
-  age = ""
-  phone = ""
-  wallet = ""
-  editMode = false
-  errorOnEmail = false
-  errorOnUsername = false
+  username = "";
+  firstname = "";
+  lastname = "";
+  age = "";
+  phone = "";
+  wallet = "";
+  editMode = false;
+  error = 'null';
+  
 
   /**
    * Initial code ran when component is loaded. 
@@ -75,13 +76,13 @@ export class ProfileComponent implements OnInit {
         params
       }
     ).subscribe(resData => {
-      this.username = resData.username
-      this.firstname = resData.first_name
-      this.lastname = resData.last_name
-      this.age = resData.age
-      this.phone = resData.phone
-      this.email = resData.email
-      this.wallet = resData.wallet
+      this.username = resData.username;
+      this.firstname = resData.first_name;
+      this.lastname = resData.last_name;
+      this.age = resData.age;
+      this.phone = resData.phone;
+      this.email = resData.email;
+      this.wallet = resData.wallet;
     });
   }
 
@@ -95,26 +96,25 @@ export class ProfileComponent implements OnInit {
    * @param form The user submitted form in order to change password.
    */
   onSaveChanges(form: NgForm) {
-    let user_id = this.user_id
+    let user_id = this.user_id;
     let email = form.value.email;
     let phone = form.value.phone;
     let old_password = form.value.password;
     let new_password = form.value.newpass;
     let newpass_conf = form.value.newpass_conf;
-    this.passwordError = false
-    this.passwordChanged = false
+    this.passwordError = false;
+    this.passwordChanged = false;
     if (new_password != newpass_conf){
-      this.passwordError = true
+      this.passwordError = true;
     }else{
       this.HttpClient.put<ResponseInterface>(
         '/api/editpass',
         {user_id: user_id, email: this.email, old_password : old_password, new_password: new_password }
       ).subscribe(resData => {
         if (resData.status == "fail"){
-          this.passwordError = true
+          this.passwordError = true;
         }else if (resData.status == "success"){
-          this.passwordChanged = true
-
+          this.passwordChanged = true;
         }
       });
     }
@@ -127,99 +127,50 @@ export class ProfileComponent implements OnInit {
    * @param form User submitted form.
    */
   onSaveProfile(form: NgForm){
-    this.errorOnEmail = false;
-    this.errorOnUsername = false;
     let email = form.value.email;
-    let needToCheckEmail = true;
     if(email.empty){
-      email = this.email
-      needToCheckEmail = false
-    }else{
-      needToCheckEmail = true
+      email = this.email;
     }
 
     let first_name = form.value.first_name;
     if(first_name.empty){
-      first_name = this.firstname
+      first_name = this.firstname;
     }
 
     let last_name = form.value.last_name;
     if(last_name.empty){
-      last_name = this.lastname
+      last_name = this.lastname;
     }
 
     let username = form.value.username;
     if(username.empty){
-      username = this.username
+      username = this.username;
     }
 
     let phone = form.value.phone;
     if(phone.empty){
-      phone = this.phone
+      phone = this.phone;
     }
 
-    if(needToCheckEmail){
-      const params = new HttpParams().append('email', email).append("username",username);
-      this.HttpClient.get<ResultData>(
-        '/api/check-emails_user',
-        {
-          params
-        }
-      ).subscribe(resData => {
-        this.check = ((resData.Result1) || (resData.Result2))
-
-        if(!this.check){
-          this.updateUser(username,email,first_name,last_name,phone)
-          this.editMode = false
-
-        }else{
-          if (resData.Result1){
-            this.errorOnEmail = true
-            if (!resData.Result2){
-              this.updateUser(username,this.email,first_name,last_name,phone)
-              this.username = username
-            }
-
-          }
-          if (resData.Result2){
-            this.errorOnUsername = true
-            if (!resData.Result1){
-              this.updateUser(this.username,email,first_name,last_name,phone)
-              this.email = email
-            }
-          }
-          this.editMode = false
-
-        }
-      });
-    }else{
-      this.updateUser(username,email,first_name,last_name,phone)
-      this.editMode = false
-    }
-  }
-
-  /**
-   * Sends an http 'PUT' request with validated credentials to the 
-   * '/api/edituser' route in order to edit the requested user credentials.
-   * 
-   * @param username Desired username
-   * @param email Desired email
-   * @param first_name Desired first name
-   * @param last_name Desired last name
-   * @param phone desired Phone number
-   */
-  updateUser(username:string, email:string, first_name:string, last_name:string, phone:string){
-    this.HttpClient.put(
+    this.HttpClient.put<any>(
       '/api/edituser',
-      {user_id: this.user_id, username: username, email: email, first_name: first_name, last_name : last_name, phone : phone}
-    ).subscribe();
-    const user = new User(
-      email,
-      this.authService.user.getValue()!.id,
-      this.authService.user.getValue()!.wallet,
-      this.authService.user.getValue()!.lender
-    );
-    localStorage.setItem('userData', JSON.stringify(user));
+      {user_id: this.user_id, username: username, email: email, 
+        first_name: first_name, last_name : last_name, phone : phone}
+    ).subscribe(resData => {
+      if(resData.Error){
+        this.error = resData.Error; 
+      }
+      else{
+        const user = new User(
+          email,
+          this.authService.user.getValue()!.id,
+          this.authService.user.getValue()!.wallet,
+          this.authService.user.getValue()!.lender
+        );
+        this.error = 'null';
+        localStorage.setItem('userData', JSON.stringify(user));
+        window.location.reload();
+      }
+    });
   }
-
 }
